dec00f87ec85d9c14adabbc06dc0fad074f8b8a4
Artur <artur.13.goncalves@gmail.com>
2023-10-26 00:34:54 +0100

    up

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,27 +6,33 @@
 /*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 19:55:42 by ade-pinh          #+#    #+#             */
-/*   Updated: 2023/10/26 00:14:13 by ade-pinh         ###   ########.fr       */
+/*   Updated: 2023/10/26 00:33:21 by ade-pinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	fr_send_bit(int pid, char c)
+void	ft_send(int pid, int i)
 {
-
+	if (i)
+		kill(pid, SIGUSR2);
+	else
+		kill(pid, SIGUSR1);
+	usleep(100);
 }
 
 int	main(int ac, char **av)
 {
 	static int	pid;
+	int			i;
 
 	if (ac < 3)
 		return (0);
 	pid = ft_atoi(av[1]);
-	while (av[2] && *av[2])
+	i = 2;
+	while (av[i] && i < ac)
 	{
-
+		ft_send(pid, ft_atoi(av[i++]));
 	}
 	return (1);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,30 +6,43 @@
 /*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 19:56:01 by ade-pinh          #+#    #+#             */
-/*   Updated: 2023/10/26 00:13:42 by ade-pinh         ###   ########.fr       */
+/*   Updated: 2023/10/26 00:28:51 by ade-pinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int signal)
+void	signal_handler(int bit)
 {
-
+	if (bit == SIGINT)
+	{
+		write(1, "\nServer Closed!\n", 16);
+		exit(EXIT_SUCCESS);
+	}
+	if (bit == SIGUSR1)
+	{
+		write(1, "0", 1);
+	}
+	if (bit == SIGUSR2)
+	{
+		write(1, "1", 1);
+	}
+	signal(bit, signal_handler);
 }
 
 int	main(void)
 {
 	pid_t				pid;
 
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	signal(SIGINT, signal_handler);
+
 	pid = getpid();
 	write(1, "Server PID: ", ft_strlen("Server PID: "));
 	ft_putnbr_fd(pid, 1);
 	write(1, "\n", 1);
 	while (1)
-	{
-		signal(SIGUSR1, signal_handler);
-		signal(SIGUSR2, signal_handler);
 		pause();
-	}
 	return (EXIT_SUCCESS);
 }
